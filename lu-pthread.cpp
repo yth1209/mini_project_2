25eee09efd53440ecf1a4d9f27e51bafb47e78a6
callee <yth001209@gmail.com>
2025-04-28 14:18:14 +0900

    lu_decomposition_parallel done

--- lu-pthread.cpp
+++ lu-pthread.cpp
@@ -9,8 +9,11 @@
 #include <functional>
 #include <map>
 #include <queue>
+#include <atomic>
 
 using namespace std;
+
+void decomposed_A_to_L_U(std::vector<std::vector<double>>& A, vector<std::vector<double>>&L, vector<std::vector<double>>&U, int n);
 
 void print_matrix(const std::vector<std::vector<double>>& mat, const char* name) {
     printf("%s:\n", name);
@@ -71,6 +74,8 @@ void lu_decomposition_seq(std::vector<std::vector<double>>& A,
         }
     }
 
+    decomposed_A_to_L_U(A_copy, L, U, n);
+
     auto t1 = chrono::steady_clock::now();
     chrono::duration<double> elapsed = t1 - t0;
     cout << "Elapsed time: " 
@@ -78,58 +83,53 @@ void lu_decomposition_seq(std::vector<std::vector<double>>& A,
 }
 
 
-struct PivotArgs {
-    double& max_v;
-    int& k_prime;
+struct DecompArgs {
+    double pivot;
     vector<vector<double>>& A_copy;
     int start;
     int end;
     int k;
+    int n;
 };
 
-pthread_mutex_t pivot_lock = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t pivot_cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t decomp_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t decomp_cond = PTHREAD_COND_INITIALIZER;
 pthread_cond_t main_thread_cond = PTHREAD_COND_INITIALIZER;
-queue<PivotArgs*> pivot_task_queue;
+queue<DecompArgs*> decomp_task_queue;
 bool is_work_done = false;
-int remaining_tasks = 0;
+atomic<int> remaining_tasks(0);
 
-void* pivot_worker(void* arg){
+void* decomp_worker(void* arg){
     while(!is_work_done){
-        pthread_mutex_lock(&pivot_lock);
-        while(pivot_task_queue.empty()){
-            pthread_cond_wait(&pivot_cond, &pivot_lock);
+        pthread_mutex_lock(&decomp_lock);
+        while(decomp_task_queue.empty()){
+            pthread_cond_wait(&decomp_cond, &decomp_lock);
         }
         // cout << "Awake Pivot Worker Done " << endl;
 
-        PivotArgs* args = pivot_task_queue.front();
+        DecompArgs* args = decomp_task_queue.front();
         if(args == nullptr){
-            pthread_mutex_unlock(&pivot_lock);
+            pthread_mutex_unlock(&decomp_lock);
             continue;
         }
-        pivot_task_queue.pop();
-        pthread_mutex_unlock(&pivot_lock);
+        decomp_task_queue.pop();
+        pthread_mutex_unlock(&decomp_lock);
         
-        double local_max = 0.0;
-        int local_k_prime = args->k;
-
-        for(int i = args->start; i < args->end; i++){
-            double abs_A_ik = abs(args->A_copy[i][args->k]);
-            if (abs_A_ik > local_max) {
-                local_max = abs_A_ik;
-                local_k_prime = i;
+        for (int i = args->start; i < args->end; i++) {
+            args->A_copy[i][args->k] /= args->pivot;
+            double L_ik = args->A_copy[i][args->k];
+      
+            auto& A_i = args->A_copy[i];
+      
+            for (int j = args->k + 1; j < args->n; j++) {
+                A_i[j] -= L_ik * args->A_copy[args->k][j];
             }
         }
 
-        pthread_mutex_lock(&pivot_lock);
-        if(local_max !=0.0 && local_max > args->max_v){
-            args->max_v = local_max;
-            args->k_prime = local_k_prime;
-        }
         remaining_tasks--;
-        pthread_cond_signal(&main_thread_cond);
-        pthread_mutex_unlock(&pivot_lock);
         delete args;
+
+        pthread_cond_signal(&main_thread_cond);
     }
 
     return nullptr;
@@ -152,13 +152,13 @@ void lu_decomposition_parallel(std::vector<std::vector<double>>& A,
 
     pthread_t pivot_threads[t];
 
-    pthread_cond_init(&pivot_cond, NULL);
+    pthread_cond_init(&decomp_cond, NULL);
     pthread_cond_init(&main_thread_cond, NULL);
-    pthread_mutex_init(&pivot_lock, NULL);
+    pthread_mutex_init(&decomp_lock, NULL);
 
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
     for(int i = 0; i < t; i++){
-        pthread_create(&pivot_threads[i], &attr, pivot_worker, NULL);
+        pthread_create(&pivot_threads[i], &attr, decomp_worker, NULL);
     }
 
     for (int k = 0; k < n; k++) {
@@ -166,31 +166,13 @@ void lu_decomposition_parallel(std::vector<std::vector<double>>& A,
         double max_v = 0.0;
         int k_prime = k;
 
-        int stride = max(1, (n-k)/t);
-        for(int tid = 0; tid < t; tid++){
-            int start = k + (stride * tid);
-            int end = min(n, start + stride);
-    
-            pthread_mutex_lock(&pivot_lock);
-            PivotArgs* args = new PivotArgs{max_v, k_prime, A_copy, start, end, k};
-            pivot_task_queue.push(args);
-
-            remaining_tasks++;
-            pthread_cond_signal(&pivot_cond);
-            pthread_mutex_unlock(&pivot_lock);
-        }
-
-
-        pthread_mutex_lock(&pivot_lock);
-        while(remaining_tasks>0){
-            pthread_cond_signal(&pivot_cond);
-            pthread_cond_wait(&main_thread_cond, &pivot_lock);
-        }
-        pthread_mutex_unlock(&pivot_lock);
-
-        if(!pivot_task_queue.empty()){
-            cerr << "Error: Task queue not empty" << endl;
-            exit(-1);
+        // Find the maximum element in the k-th column
+        for (int i = k; i < n; i++) {
+            double abs_A_ik = abs(A_copy[i][k]);
+            if (abs_A_ik > max_v) {
+                max_v = abs_A_ik;
+                k_prime = i;
+            }
         }
 
         if (max_v == 0.0) {
@@ -208,24 +190,40 @@ void lu_decomposition_parallel(std::vector<std::vector<double>>& A,
         double pivot = A_copy[k][k];
         const vector<double>& A_k = A_copy[k];
     
-        // for (int i = k + 1; i < n; i++) {
-        //     A_copy[i][k] = A_copy[i][k] / pivot;
-        //     double L_ik = A_copy[i][k];
-      
-        //     auto& A_i = A_copy[i];
-      
-        //     for (int j = k+1; j < n; j++) {
-        //         A_i[j] -= L_ik * A_k[j];
-        //     }
-        // }
+        //define total number of tasks
+        int task_num = t * 1;
+
+        //calculate strid with ceiling
+        int stride = max(1, (n-k-1 + task_num - 1)/task_num);
+        for(int tid = 0; tid < task_num; tid++){
+            int start = k+1 + (stride * tid);
+            int end = min(n, start + stride);
+
+            DecompArgs* args = new DecompArgs{pivot, A_copy, start, end, k, n};
+            decomp_task_queue.push(args);
+
+            remaining_tasks++;
+        }
+        pthread_cond_broadcast(&decomp_cond);
+
+        pthread_mutex_lock(&decomp_lock);
+        while(remaining_tasks>0){
+            // pthread_cond_signal(&decomp_cond);
+            pthread_cond_wait(&main_thread_cond, &decomp_lock);
+        }
+        pthread_mutex_unlock(&decomp_lock);
+
+        if(!decomp_task_queue.empty()){
+            cerr << "Error: Task queue not empty" << endl;
+            exit(-1);
+        }
     }
 
     is_work_done = true;
 
-    pthread_cond_broadcast(&pivot_cond);
-    // for(int i = 0; i < t; i++){
-    //     pthread_join(pivot_threads[i], NULL);
-    // }
+    pthread_cond_broadcast(&decomp_cond);
+
+    decomposed_A_to_L_U(A_copy, L, U, n);
 
     auto t1 = chrono::steady_clock::now();
     chrono::duration<double> elapsed = t1 - t0;
@@ -266,4 +264,22 @@ int main(int argc, char* argv[]) {
     }
 
     return 0;
+}
+
+
+void decomposed_A_to_L_U(std::vector<std::vector<double>>& A, std::vector<std::vector<double>>&L, std::vector<std::vector<double>>&U, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i > j) {
+                L[i][j] = A[i][j];
+                U[i][j] = 0;
+            } else if (i == j) {
+                L[i][j] = 1;
+                U[i][j] = A[i][j];
+            } else {
+                L[i][j] = 0;
+                U[i][j] = A[i][j];
+            }
+        }
+    }
 }
